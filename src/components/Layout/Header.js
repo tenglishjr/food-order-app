98366ad7d93f0ecebf95000021b6dde21a8529ce
tenglishjr <timothy.english.jr@gmail.com>
2021-07-14 21:45:52 -0500

    Add | Cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react';
 
 import classes from '../../styling/Header.module.css';
 import mealsImg from '../../assets/meals.jpeg';
+import Button from '../Layout/HeaderCartButton';
 
 const Header = props => {
     
@@ -9,7 +10,7 @@ const Header = props => {
         <Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <Button>Cart</Button>
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImg} alt="A table full of delicious food!"/>
