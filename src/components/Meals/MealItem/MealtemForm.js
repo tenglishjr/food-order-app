58ff7baaf614313c6fb0e059ac1ba39baecc3923
tenglishjr <timothy.english.jr@gmail.com>
2021-07-14 23:48:06 -0500

    Add | Meal item form components

--- src/components/Meals/MealItem/MealtemForm.js
+++ src/components/Meals/MealItem/MealtemForm.js
@@ -1,5 +1,21 @@
+import classes from '../../../styling/MealItemForm.module.css';
+import Input from "../../UI/Input";
 
-
-const MealItemForm = props => {};
+const MealItemForm = props => {
+    return <form className={classes.form}>
+        <Input
+            label="Quantity"
+            input={{
+                id: 'amount_' + props.id,
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+            }}
+        />
+        <button>+ Add</button>
+    </form>
+};
 
 export default MealItemForm;
