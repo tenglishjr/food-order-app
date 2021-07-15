c27d8cd700971fdff7293d824dae66a2e65e9fca
tenglishjr <timothy.english.jr@gmail.com>
2021-07-15 00:15:11 -0500

    Add | Cart modal 'close' functionality

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,7 +2,7 @@ import {Fragment} from 'react';
 
 import classes from '../../styling/Header.module.css';
 import mealsImg from '../../assets/meals.jpeg';
-import Button from '../Layout/HeaderCartButton';
+import HeaderCartButton from '../Layout/HeaderCartButton';
 
 const Header = props => {
     
@@ -10,7 +10,7 @@ const Header = props => {
         <Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <Button>Cart</Button>
+                <HeaderCartButton onClick={props.onShowCart}>Cart</HeaderCartButton>
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImg} alt="A table full of delicious food!"/>
